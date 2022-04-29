e928f4e5ef6463640874aa43c1808a08f9ff670e
Rodrigogarciactg <100664773+Rodrigogarciactg@users.noreply.github.com>
2022-04-29 14:00:41 -0700

    Pokemon-api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+
+// Notes are from assignment for future reference but did the assignment folloiwng the example
+// in the "useEffect" secion of my notes
 
 function App() {
+  // We expect an array of pokemon objects back and so we will
+  //    choose an empty array as the initial value to be held
+  //    in state
+  const [pokemon, setPokemon] = useState([]);
+  // this will run immediately after the JSX is rendered
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=807")
+      .then((response) => {
+        // this will return the data from the response object
+        //    in a json format
+        return response.json();
+      })
+      .then((response) => {
+        // store the json converted data in state so it can be displayed
+        setPokemon(response.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  // The empty array as the second parameter makes it so that useEffect
+  //    does NOT run more than once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="fetch">Fetch Pokemon</h1>
+      <ul>
+        {pokemon.map((pokemonObj, index) => {
+          return <li key={index}>{pokemonObj.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
